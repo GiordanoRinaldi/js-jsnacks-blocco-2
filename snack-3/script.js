b97f2484b137a9deda30cda492847e33e1a23fbb
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-08-27 13:15:22 +0200

    invitati senza esclusione doppio

---
+++ snack-3/script.js
@@ -0,0 +1,17 @@
+//Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+//lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+var nome = ["marco ", "luigi ", "stefano "];
+var cognome = [ "rossi", "bianchi", "neri"];
+var invitati = []
+
+for (var i = 0; i < 3; i++) {
+    var nomecognome = nome[Math.floor(Math.random()*3)] + cognome[Math.floor(Math.random()*3)];
+    invitati.push(nomecognome)
+}
+
+
+
+console.log(invitati);
+
+
