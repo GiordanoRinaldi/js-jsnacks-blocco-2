31745ee37fe9b556c1a1aa5c0a31c6e1b671411e
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-08-27 12:20:43 +0200

    snack 2

---
+++ snack-2/script.js
@@ -0,0 +1,12 @@
+// inserisci un numero se pari stampa se dispari stampa quello dopo
+
+var numero = parseInt(prompt("inserisci un numero"));
+if (isNaN(numero)) {
+    var numero = parseInt(prompt("inserisci un c***o di numero"))
+}
+
+if ( numero%2 == 0 ) {
+    alert(numero)
+} else {
+    alert(numero + 1 )
+}
