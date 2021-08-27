6ab2defbf26de5e13672acd9feab166359777ab6
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-08-27 12:10:39 +0200

    snack 1 while

--- snack-1/scrpt.js
+++ snack-1/scrpt.js
@@ -2,13 +2,24 @@
 //Il programma stampa la somma di tutti i numeri inseriti.
 //Esegui questo programma in due versioni, con il for e con il while. 
 
-var somma = 0;
+//var somma = 0;
 
-for (var i = 1; i <= 5; i++) { 
-    var numeroUtente = parseInt(prompt("inserisci un numero")) ;
-    var somma = somma + numeroUtente;
+//for (var i = 1; i <= 5; i++) { 
+//    var numeroUtente = parseInt(prompt("inserisci un numero")) ;
+//    var somma = somma + numeroUtente;
+//}
+
+//console.log(somma);
+
+//  while
+
+var somma = 0;
+var i = 0;
+
+while (i < 5) {
+    var numero = parseInt(prompt("inserisci un numero"))
+    var somma = somma + numero;
+    i++;
 }
 
 console.log(somma);
-
-//
